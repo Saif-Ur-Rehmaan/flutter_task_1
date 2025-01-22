0cd4d7aa6ccbeed4b4fbbfdb3d6df2fc329c0000
Saif-Ur-Rehmaan <FSDev.SaifUrRehman@gmail.com>
2025-01-23 00:54:41 +0500

    C R done

--- lib/addDataScreen.dart
+++ lib/addDataScreen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 class AddDataScreen extends StatefulWidget {
@@ -8,23 +9,39 @@ class AddDataScreen extends StatefulWidget {
 }
 
 class _AddDataScreenState extends State<AddDataScreen> {
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  submit() {
+    final DatabaseReference database = FirebaseDatabase.instance.ref();
+
+    var k=database.child('users').push().key;
+    database.child('users/$k').set({
+      'name': nameController.text,
+      'email': emailController.text,
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Add User Screen"),),
+      appBar: AppBar(
+        title: Text("Add User Screen"),
+      ),
       body: Column(
         children: [
           TextField(
+            controller: nameController,
             decoration: InputDecoration(
               label: Text('Name'),
             ),
           ),
           TextField(
+            controller: emailController,
             decoration: InputDecoration(
               label: Text('email'),
             ),
           ),
-          ElevatedButton(onPressed: (){}, child: Text('Add Data'))
+          ElevatedButton(onPressed: submit, child: Text('Add Data'))
         ],
       ),
     );
